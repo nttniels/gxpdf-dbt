521b0ab7fe3ea6f9646100bc6ad55778ec376209
Niels <niels.van.gaans@bs.nttdata.com>
2024-01-05 13:44:59 +0100

    init from example directory

--- macros/generate_model_yaml.sql
+++
@@ -1,5 +0,0 @@
--- example of using codegen to create yaml for a given model
-
-{{ codegen.generate_model_yaml(
-    model_name='stg_team'
-) }}
--- models/staging/stg_event_type.sql
+++
@@ -1,8 +0,0 @@
-with event_type as (
-    select * from {{ source('fifa', 'event_type') }}
-)
-
-select 
-    id as event_type_id
-    , replace(lower(name),' ','_') as event_type_name
-from event_type
