e6834462e4f6845c20b8c9156c322648365b0f8f
Niels van Gaans <niels.van.gaans@bs.nttdata.com>
2024-01-09 14:28:43 +0000

    sap source

--- models/20_bronze_polished/SAP_S4H_LSD200/makt.sql
+++
@@ -1,8 +0,0 @@
-
-select 
-    -- matnr as number,
-    -- spras as string,
-    -- maktx as string,
-    -- maktg as string
-    *
-from {{ source('SAP_S4H_LSD200', 'makt') }}
--- models/40_gold/sales order.sql
+++
@@ -1,8 +0,0 @@
-
-select 
-    matnr as number,
-    spras as string(1),
-    maktx as string(40),
-    maktg as string(40)
-    primary key (matnr, spras)
-from {{ source('SAP_S4H_LSD200', 'makt') }}
