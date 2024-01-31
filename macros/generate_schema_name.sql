214535977ef98da186d60907c72ba265dfbf3aaa
Niels van Gaans <niels.van.gaans@bs.nttdata.com>
2024-01-31 10:52:43 +0000

    custom schemas

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,26 +1,26 @@
 
--- remove schema prefixes for quality and production environment
+{# remove schema prefixes for quality and production environment #}
 
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
 
-    -- remove schema prefixes if required by environment
+    {# remove schema prefixes if required by environment #}
     {%- if env_var('DBT_DEFAULT_SCHEMA') == 'OFF' -%}
         {%- if custom_schema_name is none -%}
-            -- return no default schema name
+            {# return no default schema name #}
         {%- else -%}
-            -- only return custom schema name
+            {# only return custom schema name #}
             {{ custom_schema_name | trim }}
         {%- endif -%}
 
-    -- default macro
+    {# default macro #}
     {%- else -%}
         {%- if custom_schema_name is none -%}
-            -- return no default schema name
+            {# return no default schema name #}
             {{ default_schema }}
         {%- else -%}
-            -- only return custom schema name
+            {# only return custom schema name #}
             {{ default_schema }}_{{ custom_schema_name | trim }}
         {%- endif -%}
     {%- endif -%}
