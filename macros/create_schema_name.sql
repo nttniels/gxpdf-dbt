bfdd9c2412bf35826861156d9c3e056954843657
Niels van Gaans <niels.van.gaans@bs.nttdata.com>
2024-01-31 15:11:49 +0000

    custom schema

---
+++ macros/create_schema_name.sql
@@ -0,0 +1,20 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+        {{ default_schema }}
+    {%- else -%}
+
+        {% set s1 = default_schema %}
+        {% set s2 = custom_schema_name | trim %}
+        {% set s3 = s1 ~ '_' ~ s2 %}
+
+        {% if 'PUBLIC_' in s3 -%}
+            {{ s3 | replace('PUBLIC_','') }}
+        {%- else -%}
+            {{ s3 }}
+        {%- endif -%}
+
+    {%- endif -%}
+	
+{%- endmacro %}
