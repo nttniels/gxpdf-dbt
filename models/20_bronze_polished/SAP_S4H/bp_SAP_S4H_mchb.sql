3cdcf5630319fb8da7ea26a17dbbf4a32c8b4878
Danny Groothuis <Danny.Groothuis@nttdata.com>
2024-02-28 10:29:22 +0000

    numeric data type updated in model

--- models/20_bronze_polished/SAP_S4H/bp_SAP_S4H_mchb.sql
+++ models/20_bronze_polished/SAP_S4H/bp_SAP_S4H_mchb.sql
@@ -12,7 +12,7 @@ bronze_polished_model as (
         charg::string(10) as charge_id,
         werks::string (4) as plant_id,
         lgort::string (4) as sloc_id, 
-        clabs::numeric (13) as unrestrict_qty,
+        clabs::number (13) as unrestrict_qty,
         vfdat::date as expiry_date
     from bronze_raw_table
 )
